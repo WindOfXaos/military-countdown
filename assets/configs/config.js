4272c44a6c545ac0c6447f82d046637a3edc5ceb
Moaaz W. Lotfi <52869398+WindOfXaos@users.noreply.github.com>
2023-08-30 16:24:01 +0300

    modify config.js (correct my discharge date)

--- assets/configs/config.js
+++ assets/configs/config.js
@@ -1,5 +1,5 @@
 const config = {
     department: "INTERIOR", // INTERIOR or DEFENSE
-    date: "2023-12-1", // discharge date eg. "2025-2-1"
+    date: "2023-11-25", // discharge date eg. "2025-2-1"
     username: "WindOfXaos" // your exact username on github.com
 }
